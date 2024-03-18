120318cb6ba6cb49f49441a36376129ef6812d2d
s24051 <s24051@pjwstk.edu.pl>
2024-03-18 21:14:50 +0100

    +metoda statyczna 'average' do wyliczenia sredniej

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿Console.WriteLine("Zadanie pierwsze z APBD!");
-Console.WriteLine("Pierwsza aplikacja dotnet!");
+﻿public class Program
+{
+    static double average(int[] ints)
+    {
+        double vLocal = 0;
+        foreach (int i in ints)
+        {
+            vLocal += (double)i / ints.Length;
+        }
+        return vLocal;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Zadanie pierwsze z APBD!");
+        Console.WriteLine("Pierwsza aplikacja dotnet!");
+        Console.WriteLine("Średnia z 5 4 2 8 7: " + average([5, 4, 2, 8, 7]));
+    }
+}
+
